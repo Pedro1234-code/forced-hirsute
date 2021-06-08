8381360f14d5270bac24c9a98254d5198e1d6d9d
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-06-07 21:16:39 -0300

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -1,41 +1,22 @@
 #!/bin/bash
 
 echo Starting Ubuntu 21.04 Forced Upgrade
-
 UBUNTU=ubuntu-base-21.04-base-amd64.tar.gz
-
 wget http://cdimage.ubuntu.com/ubuntu-base/releases/hirsute/release/ubuntu-base-21.04-base-amd64.tar.gz
-
 tar -xzf $UBUNTU
-
 cd etc
-
 cd apt
-
 cp /etc/apt/sources.list /etc/sources.list.bak
-
 rm -rf /etc/apt/sources.list
-
 cp sources.list /etc/apt/
-
 cp trusted.gpg.d/* /etc/apt/trusted.gpg.d/
-
 cd ..
-
 rm -rf /etc/os-release
-
 cp os-release /etc/
-
 apt update
-
 apt update 
-
 apt upgrade -y
-
 rm -rf /var/lib/dpkg/info/postfix.postinst
-
 apt upgrade
-
 echo Ubuntu 21.04 Installed! Reboot your system to complete the upgrade.
-
 echo Credits: Pedro1234-code (https://github.com/Pedro1234-code)
